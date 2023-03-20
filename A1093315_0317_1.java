61a5fe255209f980f25c60ef7ffd3700643a6831
a1093315 <125523140+a1093315@users.noreply.github.com>
2023-03-20 14:01:39 +0800

    Add files via upload

---
+++ A1093315_0317_1.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+public class A1093315_0317_1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the 'n' to calculate (1~n)*(1~m):");
+        int n=input.nextInt();
+        while(n<0)
+        {
+            System.out.println("Invalid input. Please enter a number which is larger than 1.");
+            System.out.print("Enter the 'n' to calculate (1~n)*(1~m):");
+            n=input.nextInt();
+        }
+        System.out.print("Enter the 'm' to calculate (1~n)*(1~m):");
+        int m=input.nextInt();
+        while(m<0)
+        {
+            System.out.println("Invalid input. Please enter a number which is larger than 1.");
+            System.out.print("Enter the 'm' to calculate (1~n)*(1~m):");
+            m=input.nextInt();
+        }
+        int[][] arr = new int[n][m];
+        for(int row=0; row<n; row++)
+        {
+            for(int col=0; col<m; col++)
+                arr[row][col]=(row+1)*(col+1);
+        }
+        int countr=1;
+        int countc=1;
+        int maxnum=n*m;
+        boolean toolarge=false;
+        for(int[] row:arr)
+        {
+            countc=1;
+            for(int col:row)
+            {
+                switch(digit(maxnum))
+                {
+                    case 1:
+                        System.out.printf("<%d * %d = %d> ", countr, countc, arr[countr-1][countc-1]);
+                        break;
+                    case 2:
+                        System.out.printf("<%2d * %2d = %2d> ", countr, countc, arr[countr-1][countc-1]);
+                        break;
+                    case 3:
+                        System.out.printf("<%3d * %3d = %3d> ", countr, countc, arr[countr-1][countc-1]);
+                        break;
+                    case 4:
+                        System.out.printf("<%4d * %4d = %4d> ", countr, countc, arr[countr-1][countc-1]);
+                        break;
+                    case 5:
+                        System.out.printf("<%3d * %3d = %5d> ", countr, countc, arr[countr-1][countc-1]);
+                        break;
+                    default:
+                        toolarge=true;
+                        break;
+                }
+                if(toolarge==true)
+                    break;
+                countc++;
+            }
+            if(toolarge==true)
+                break;
+            countr++;
+            System.out.println();
+        }
+        if(toolarge==true)
+        {
+            System.out.println("It's difficult to show an orderly calculation table because numbers are too large.");
+            System.out.println("Maybe you should try numbers which are smaller than 100 * 100.");
+        }
+    }
+    public static int digit(int num)
+    {
+        int i=1;
+        while(num/10>0)
+        {
+            num/=10;
+            i+=1;
+        }
+        return i;
+    }
+}
